7751ccf136ad71bea2ba3c226e1b5f308e6f1b04
RG <rg@innosystec.de>
2024-07-10 16:23:39 +0200

    adds MovementType to different UnitTypes

--- src/main/java/de/nurrobin/enums/UnitType.java
+++ src/main/java/de/nurrobin/enums/UnitType.java
@@ -1,22 +1,32 @@
 package de.nurrobin.enums;
 
 public enum UnitType {
-    INFANTRY,
-    MECH_INFANTRY,
-    RECON,
-    TANK,
-    MEDIUM_TANK,
-    APC,
-    ARTILLERY,
-    ROCKETS,
-    ANTI_AIR,
-    MISSILES,
-    FIGHTER,
-    BOMBER,
-    BATTLE_COPTER,
-    TRANSPORT_COPTER,
-    BATTLESHIP,
-    CRUISER,
-    SUBMARINE,
-    LANDER
+    INFANTRY(MovementType.FOOT),
+    MECH_INFANTRY(MovementType.BOOTS),
+    RECON(MovementType.TIRES),
+    TANK(MovementType.TREADS),
+    MEDIUM_TANK(MovementType.TREADS),
+    APC(MovementType.TREADS),
+    ARTILLERY(MovementType.TREADS),
+    ROCKETS(MovementType.TREADS),
+    ANTI_AIR(MovementType.TREADS),
+    MISSILES(MovementType.TREADS),
+    FIGHTER(MovementType.AIR),
+    BOMBER(MovementType.AIR),
+    BATTLE_COPTER(MovementType.AIR),
+    TRANSPORT_COPTER(MovementType.AIR),
+    BATTLESHIP(MovementType.SEA),
+    CRUISER(MovementType.SEA),
+    SUBMARINE(MovementType.SEA),
+    LANDER(MovementType.LANDER);
+
+    private final MovementType movementType;
+
+    UnitType(MovementType movementType) {
+        this.movementType = movementType;
+    }
+
+    public MovementType getMovementType() {
+        return movementType;
+    }
 }
