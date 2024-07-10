dffc8ef26718d3950445cfdca170bcf8c97e4642
RG <rg@innosystec.de>
2024-07-10 16:33:16 +0200

    refactors Tile class

--- src/main/java/de/nurrobin/model/Tile.java
+++ src/main/java/de/nurrobin/model/Tile.java
@@ -8,9 +8,9 @@ import javafx.scene.image.Image;
 
 public class Tile {
     private final int tileID;
-    private final Terrain terrain;
+    private Terrain terrain;
     private final boolean hasObject;
-    private final Image backgroundImage;
+    private Image backgroundImage;
     private Image objectImage;
 
     private final int defenseBonus;
@@ -20,11 +20,39 @@ public class Tile {
     public Tile(int tileID) {
         this.tileID = tileID;
         this.hasObject = (tileID != 0 && tileID != 3);
-        if (tileID != 3 && tileID != 5) {
-            this.backgroundImage = new Image("/tiles/textures/plain.png");
-        } else {
-            this.backgroundImage = new Image("/tiles/textures/sea.png");
+        initBackgroundImage(tileID);
+        initTerrain(tileID);
+        initObjectImage(tileID);
+        this.defenseBonus = this.terrain.getType().getDefenseBonus();
+        this.movementCosts = this.terrain.getType().getMovementCosts();
         }
+
+    private void initObjectImage(int tileID) {
+        if (hasObject) {
+            this.objectImage = switch (tileID) {
+                case 1 -> new Image("/tiles/textures/wood.png");
+                case 2 -> new Image("/tiles/textures/mountain.png");
+                case 4, 5 -> new Image("/tiles/textures/road.png");
+                case 7 -> new Image("/buildings/textures/HQ-Orange.png");
+                case 8 -> new Image("/buildings/textures/HQ-Blue.png");
+                case 9,10,11 -> new Image("/buildings/textures/City-Neutral.png");
+//                case 10 -> new Image("/buildings/textures/City-Orange.png");
+//                case 11 -> new Image("/buildings/textures/City-Blue.png");
+                case 12 -> new Image("/buildings/textures/Base-Neutral.png");
+                case 13 -> new Image("/buildings/textures/Base-Orange.png");
+                case 14 -> new Image("/buildings/textures/Base-Blue.png");
+                case 15,16,17 -> new Image("/buildings/textures/Airport-Neutral.png");
+//                case 16 -> new Image("/buildings/textures/Airport-Orange.png");
+//                case 17 -> new Image("/buildings/textures/Airport-Blue.png");
+                case 18,19,20 -> new Image("/buildings/textures/Port-Neutral.png");
+//                case 19 -> new Image("/buildings/textures/Port-Orange.png");
+//                case 20 -> new Image("/buildings/textures/Port-Blue.png");
+                default -> throw new IllegalArgumentException("Unknown tile value: " + tileID);
+            };
+            }
+    }
+
+    private void initTerrain(int tileID) {
         this.terrain = switch (tileID) {
             case 0 -> new Terrain(TerrainType.PLAINS);
             case 1 -> new Terrain(TerrainType.WOODS);
@@ -48,33 +76,15 @@ public class Tile {
             case 20 -> new Terrain(TerrainType.PORT_BLUE);
             default -> throw new IllegalArgumentException("Unknown tile value: " + tileID);
         };
-        if (hasObject) {
-            this.objectImage = switch (tileID) {
-                case 1 -> new Image("/tiles/textures/wood.png");
-                case 2 -> new Image("/tiles/textures/mountain.png");
-                case 4, 5 -> new Image("/tiles/textures/road.png");
-                case 7 -> new Image("/buildings/textures/HQ-Orange.png");
-                case 8 -> new Image("/buildings/textures/HQ-Blue.png");
-                case 9,10,11 -> new Image("/buildings/textures/City-Neutral.png");
-//                case 10 -> new Image("/buildings/textures/City-Orange.png");
-//                case 11 -> new Image("/buildings/textures/City-Blue.png");
-                case 12 -> new Image("/buildings/textures/Base-Neutral.png");
-                case 13 -> new Image("/buildings/textures/Base-Orange.png");
-                case 14 -> new Image("/buildings/textures/Base-Blue.png");
-                case 15,16,17 -> new Image("/buildings/textures/Airport-Neutral.png");
-//                case 16 -> new Image("/buildings/textures/Airport-Orange.png");
-//                case 17 -> new Image("/buildings/textures/Airport-Blue.png");
-                case 18,19,20 -> new Image("/buildings/textures/Port-Neutral.png");
-//                case 19 -> new Image("/buildings/textures/Port-Orange.png");
-//                case 20 -> new Image("/buildings/textures/Port-Blue.png");
-                default -> throw new IllegalArgumentException("Unknown tile value: " + tileID);
-            };
+    }
 
-            this.defenseBonus = this.terrain.getType().getDefenseBonus();
-
-            this.movementCosts = this.terrain.getType().getMovementCosts();
-            }
+    private void initBackgroundImage(int tileID) {
+        if (tileID != 3 && tileID != 5) {
+            this.backgroundImage = new Image("/tiles/textures/plain.png");
+        } else {
+            this.backgroundImage = new Image("/tiles/textures/sea.png");
         }
+    }
 
     public int getTileID() {
         return tileID;
