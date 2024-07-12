68f947706e0d120348ef07c49c2d9533bd4992b9
Robin <robin@nurrobin.de>
2024-07-12 18:39:50 +0200

    Adds JavaDocs to Main.Java

--- src/main/java/de/nurrobin/Main.java
+++ src/main/java/de/nurrobin/Main.java
@@ -6,9 +6,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The main class for the Advance Wars-like game application. It initializes the JavaFX application.
+ */
 public class Main extends Application {
     private final Logger logger = new Logger(Main.class);
 
+    /**
+     * Starts the JavaFX application by loading the main view from an FXML file and setting up the primary stage.
+     *
+     * @param primaryStage The primary stage for this application, onto which the application scene can be set.
+     * @throws Exception if there is any issue loading the FXML file.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         logger.logDebug("Loading FXML file...");
@@ -21,6 +30,12 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * The main entry point for all JavaFX applications. The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * @param args The command line arguments passed to the application. Not used in this application.
+     */
     public static void main(String[] args) {
         launch(args);
     }
